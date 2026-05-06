67328c178c1091efd7bf73c932f38f5af5702ea5
Yevgeniy <ulchenkov2001@gmail.com>
2026-05-06 11:50:37 +0200

    [fux] readme typo in title

--- src/test/java/cz/cvut/fel/recordManagerStatisticsServer/AppTest.java
+++ src/test/java/cz/cvut/fel/recordManagerStatisticsServer/AppTest.java
@@ -2,5 +2,12 @@ package cz.cvut.fel.recordManagerStatisticsServer;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AppTest {
+
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
 }
